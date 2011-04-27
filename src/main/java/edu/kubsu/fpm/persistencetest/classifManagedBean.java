5e545294598ecc8df0e6179b1ebd636d6f85c207
sid_m <sid_m@3e0f16bf-1f82-4b49-a869-d036aacb8477>
2011-04-27 19:02:44 +0000

    Поправка в работе с Persistence

---
+++ src/main/java/edu/kubsu/fpm/persistencetest/classifManagedBean.java
@@ -0,0 +1,87 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.kubsu.fpm.persistencetest;
+
+import edu.kubsu.fpm.model.Classifier;
+
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Марина
+ */
+@ManagedBean
+@SessionScoped
+@TransactionManagement(TransactionManagementType.CONTAINER)
+public class classifManagedBean implements Serializable {
+
+    private String newClassifName;
+    private String newfactcollName;
+    private List listClassifName = new ArrayList();
+    private List listfactcollName = new ArrayList();
+
+    @PersistenceContext(unitName = "educube")
+    private EntityManager em;
+
+    public classifManagedBean() {
+    }
+
+    //    Добавляет новые классификаторы
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public String addClassif() {
+        //        Получаем экземпляр класса с заданными значениями
+        Classifier classif = new Classifier(newClassifName);
+        newClassifName = "";
+        //        Записываем значение в базу
+
+        em.persist(classif);
+        return "work_with_bd";
+    }
+
+    public List getListClassifName() {
+        return listClassifName;
+    }
+
+    public void setListClassifName(List listClassifName) {
+        this.listClassifName = listClassifName;
+    }
+
+    public List getListfactcollName() {
+        return listfactcollName;
+    }
+
+    public void setListfactcollName(List listfactcollName) {
+        this.listfactcollName = listfactcollName;
+    }
+
+    public String getNewClassifName() {
+        return newClassifName;
+    }
+
+    public void setNewClassifName(String newClassifName) {
+        this.newClassifName = newClassifName;
+    }
+
+    public String getNewfactcollName() {
+        return newfactcollName;
+    }
+
+    public void setNewfactcollName(String newfactcollName) {
+        this.newfactcollName = newfactcollName;
+    }
+
+
+}
