37ec132b08b508bede38ddcd487faede5015c6b6
marinka <marinka@3e0f16bf-1f82-4b49-a869-d036aacb8477>
2011-05-01 15:51:51 +0000

    Шаблон модуля тестирования. Не работает только persistence в тестовом примере.

--- src/main/java/edu/kubsu/fpm/TestingModule/TaskBean.java
+++ src/main/java/edu/kubsu/fpm/TestingModule/TaskBean.java
@@ -16,13 +16,40 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class TaskBean {
 
-    private String testResult = "";
-    private Integer rightAnswer = 0;
-    private Integer countAnswer = 0;
+    private String testResult = "";     //  Возвращает строчку с результатом тестирования
+    private Integer countRightAnswer = 0;    //  Содержит колличество парвильных ответов студента
+    private Integer countAnswer = 0;    //  Содержит колличество заданных вопросов
+    private String studentAnswer = "";  //  Содержит текущий ответ студента
+    private String rightAnswer;         //  Содержит текущий правильный ответ на заданный вопрос
+    private Integer currentCountQuestion;   //  Число вопросов, заданных на данный момент
+    private String currentQuestion;     //  Содержит текст текущего вопроса
 
+//    Проверяет текущий ответ студента
+    public String checkAnswer(){
+        String url = "student_test";
+//        if (rightAnswer.equals(studentAnswer)){
+//            countRightAnswer++;
+//        }
+//        if (currentCountQuestion.equals(countAnswer))
+            url = "student_mark";
+        return url;
+    }
+
+//    Формирует вывод результатов тестирования
     public String getTestResult() {
-        testResult = "Вы ответили на ".concat(this.getRightAnswer().toString()).concat(" из ").concat(this.getCountAnswer().toString());
+        testResult = "Вы ответили на ".concat(this.getCountRightAnswer().toString()).concat(" вопросов из ").concat(this.getCountAnswer().toString());
         return testResult;
+    }
+
+//   Генерирует текущий тестовый вопрос
+    public String getCurrentQuestion() {
+//        Почему-то не переводится каретка
+        currentQuestion = "Здесь будет формироваться тестовый вопрос. \n А еще нужно придумать что-то с картинками.";
+        return currentQuestion;
+    }
+
+    public void setCurrentQuestion(String currentQuestion) {
+        this.currentQuestion = currentQuestion;
     }
 
     public void setTestResult(String testResult) {
@@ -37,12 +64,36 @@ public class TaskBean {
         this.countAnswer = countAnswer;
     }
 
-    public Integer getRightAnswer() {
+    public Integer getCurrentCountQuestion() {
+        return currentCountQuestion;
+    }
+
+    public void setCurrentCountQuestion(Integer currentCountQuestion) {
+        this.currentCountQuestion = currentCountQuestion;
+    }
+
+    public String getRightAnswer() {
         return rightAnswer;
     }
 
-    public void setRightAnswer(Integer rightAnswer) {
+    public void setRightAnswer(String rightAnswer) {
         this.rightAnswer = rightAnswer;
+    }
+
+    public Integer getCountRightAnswer() {
+        return countRightAnswer;
+    }
+
+    public void setCountRightAnswer(Integer countRightAnswer) {
+        this.countRightAnswer = countRightAnswer;
+    }
+
+    public String getStudentAnswer() {
+        return studentAnswer;
+    }
+
+    public void setStudentAnswer(String studentAnswer) {
+        this.studentAnswer = studentAnswer;
     }
 
     public TaskBean(){
