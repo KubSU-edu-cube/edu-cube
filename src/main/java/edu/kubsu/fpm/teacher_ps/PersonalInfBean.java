ec41601f9167c010381b8b5a6c912c0bfdcfac0e
julanovaanna <julanovaanna@rambler.ru>
2012-04-06 11:09:50 +0400

    Изменение личной информации учителя

---
+++ src/main/java/edu/kubsu/fpm/teacher_ps/PersonalInfBean.java
@@ -0,0 +1,126 @@
+package edu.kubsu.fpm.teacher_ps;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: anna
+ * Date: 06.04.12
+ * Time: 0:06
+ * To change this template use File | Settings | File Templates.
+ */
+@ManagedBean
+@SessionScoped
+public class PersonalInfBean {
+    private String name;
+    private String surname;
+    private String patronymic;
+    private String dateOfBirth;
+    private String cityOfBirth;
+    private String currentCity;
+    private String mobTel;
+    private String homeTel;
+    private String skype;
+    private String icq;
+    private String webSite;
+    private String Adress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+
+    public String getCityOfBirth() {
+        return cityOfBirth;
+    }
+
+    public void setCityOfBirth(String cityOfBirth) {
+        this.cityOfBirth = cityOfBirth;
+    }
+
+    public String getCurrentCity() {
+        return currentCity;
+    }
+
+    public void setCurrentCity(String currentCity) {
+        this.currentCity = currentCity;
+    }
+
+    public String getMobTel() {
+        return mobTel;
+    }
+
+    public void setMobTel(String mobTel) {
+        this.mobTel = mobTel;
+    }
+
+    public String getHomeTel() {
+        return homeTel;
+    }
+
+    public void setHomeTel(String homeTel) {
+        this.homeTel = homeTel;
+    }
+
+    public String getSkype() {
+        return skype;
+    }
+
+    public void setSkype(String skype) {
+        this.skype = skype;
+    }
+
+    public String getIcq() {
+        return icq;
+    }
+
+    public void setIcq(String icq) {
+        this.icq = icq;
+    }
+
+    public String getWebSite() {
+        return webSite;
+    }
+
+    public void setWebSite(String webSite) {
+        this.webSite = webSite;
+    }
+
+    public String getAdress() {
+        return Adress;
+    }
+
+    public void setAdress(String adress) {
+        Adress = adress;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
